c7513c69412d08665fe78a93e092cd1d948ce5b0
dogue_one <doggueone@gmail.com>
2019-11-17 23:05:02 +0300

    User model, Event-User relation, registration verification incomplete, psw encryption

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require('express');
 const connectDB = require('./config/db');
 const graphqlHttp = require('express-graphql');
 const { buildSchema } = require('graphql');
+const bcrypt = require('bcryptjs');
 
 const Event = require('./models/event');
+const User = require('./models/user');
 
 const app = express();
 
@@ -25,13 +27,25 @@ app.use(
             date: String!
         }
 
+        type User {
+          _id: ID!
+          email: String!
+          password: String
+        }
+
+
+
         input EventInput {
             title: String!
             description: String!
             price: Float!
             date: String!
         }
-    
+        
+        input UserInput {
+           email: String!
+           password: String!
+        }
     
         type RootQuery {
             events: [Event!]!
@@ -39,6 +53,7 @@ app.use(
 
         type RootMutation {
             createEvent(eventInput:EventInput): Event
+            createUser(userInput: UserInput): User
         }
 
         schema {
@@ -49,7 +64,6 @@ app.use(
     // rootValue - bundle of all the resolvers we have (logic to act on incoming requests)
     rootValue: {
       events: () => {
-        //add return to indicate that this is async operation
         return Event.find()
           .then(events => {
             return events.map(event => {
@@ -61,44 +75,58 @@ app.use(
           });
       },
       createEvent: args => {
-        /* const event = {
-          _id: Math.random().toString(),
-          title: args.eventInput.title,
-          description: args.eventInput.description,
-          //+-converted to a number
-          price: +args.eventInput.price,
-          date: args.eventInput.date
-          // new Date().toISOString()
-        }; */
         const event = new Event({
           title: args.eventInput.title,
           description: args.eventInput.description,
           price: +args.eventInput.price,
-          date: new Date(args.eventInput.date)
+          date: new Date(args.eventInput.date),
+          creator: '5dd17a22dc562d39408d19ee'
         });
-        //add return to indicate that this is async operation
+        let createdEvent;
         return event
           .save()
           .then(result => {
-            console.log(result);
             //to leave out all metadata of the returned Event(_doc - property provided by mongoose)
-
-            //transform _id to normal string that is understood by graphql, mongoose shortcut _id: event.id
-
-            return { ...result._doc, _id: result._doc._id.toString() };
+            //transform _id to normal string that is understood by graphql; mongoose shortcut _id: event.id
+            createdEvent = { ...result._doc, _id: result._doc._id.toString() };
+            return User.findById('5dd17a22dc562d39408d19ee');
+          })
+          .then(user => {
+            if (!user) {
+              throw new Error('User not found.');
+            }
+            user.createdEvents.push(event);
+            return user.save();
+          })
+          .then(result => {
+            return createdEvent;
           })
           .catch(err => {
             console.log(err);
-            //graphql & express-graphql can handle that error
             throw err;
           });
-        /* http://localhost:5000/graphql
-        mutation {
-            createEvent(eventInput:{title: "A Test", description: "Does this work?", price: 9.99, date: "2019-11-16T22:19:25.011Z"}) {
-              title
-              description
+      },
+      createUser: args => {
+        return User.findOne({ email: args.userInput.email })
+          .then(user => {
+            if (user) {
+              throw new Error('User exists already.');
             }
-          } */
+            return bcrypt.hash(args.userInput.password, 12);
+          })
+          .then(hashedPassword => {
+            const user = new User({
+              email: args.userInput.email,
+              password: hashedPassword
+            });
+            return user.save();
+          })
+          .then(result => {
+            return { ...result._doc, password: null, _id: result.id };
+          })
+          .catch(err => {
+            throw err;
+          });
       }
     },
     graphiql: true
