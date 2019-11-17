2036a528ca095581fd170c9bf1ace7a7bbd73e2d
dogue_one <doggueone@gmail.com>
2019-11-17 18:20:22 +0300

    graphql & mongodb connected

--- config/db.js
+++ config/db.js
@@ -4,10 +4,13 @@ const db = config.get('mongoURI');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(db, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
+    await mongoose.connect(
+      `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@eventbook-fe7jn.gcp.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      }
+    );
 
     console.log('MongoDB Connected...');
   } catch (err) {
--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const connectDB = require('./config/db');
 const graphqlHttp = require('express-graphql');
 const { buildSchema } = require('graphql');
 
-const app = express();
+const Event = require('./models/event');
 
-const events = [];
+const app = express();
 
 // Connect Database
 connectDB();
@@ -49,10 +49,19 @@ app.use(
     // rootValue - bundle of all the resolvers we have (logic to act on incoming requests)
     rootValue: {
       events: () => {
-        return events;
+        //add return to indicate that this is async operation
+        return Event.find()
+          .then(events => {
+            return events.map(event => {
+              return { ...event._doc, _id: event.id };
+            });
+          })
+          .catch(err => {
+            throw err;
+          });
       },
       createEvent: args => {
-        const event = {
+        /* const event = {
           _id: Math.random().toString(),
           title: args.eventInput.title,
           description: args.eventInput.description,
@@ -60,9 +69,29 @@ app.use(
           price: +args.eventInput.price,
           date: args.eventInput.date
           // new Date().toISOString()
-        };
-        events.push(event);
-        return event;
+        }; */
+        const event = new Event({
+          title: args.eventInput.title,
+          description: args.eventInput.description,
+          price: +args.eventInput.price,
+          date: new Date(args.eventInput.date)
+        });
+        //add return to indicate that this is async operation
+        return event
+          .save()
+          .then(result => {
+            console.log(result);
+            //to leave out all metadata of the returned Event(_doc - property provided by mongoose)
+
+            //transform _id to normal string that is understood by graphql, mongoose shortcut _id: event.id
+
+            return { ...result._doc, _id: result._doc._id.toString() };
+          })
+          .catch(err => {
+            console.log(err);
+            //graphql & express-graphql can handle that error
+            throw err;
+          });
         /* http://localhost:5000/graphql
         mutation {
             createEvent(eventInput:{title: "A Test", description: "Does this work?", price: 9.99, date: "2019-11-16T22:19:25.011Z"}) {
