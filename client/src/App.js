b60721833a66fa432eac6115dded3da4e4ab3064
dogue_one <doggueone@gmail.com>
2019-11-20 20:38:18 +0300

    Authentication page with toggle, Hitting the API, Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+import MainNav from './components/Navigation/MainNav';
 
 import AuthPage from './pages/Auth';
 import BookingsPage from './pages/Bookings';
@@ -11,12 +12,17 @@ class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Switch>
-          <Redirect from='/' to='/auth' exact />
-          <Route path='/auth' component={AuthPage} />
-          <Route path='/events' component={EventsPage} />
-          <Route path='/bookings' component={BookingsPage} />
-        </Switch>
+        <React.Fragment>
+          <MainNav />
+          <main className='main-content'>
+            <Switch>
+              <Redirect from='/' to='/auth' exact />
+              <Route path='/auth' component={AuthPage} />
+              <Route path='/events' component={EventsPage} />
+              <Route path='/bookings' component={BookingsPage} />
+            </Switch>
+          </main>
+        </React.Fragment>
       </BrowserRouter>
     );
   }
